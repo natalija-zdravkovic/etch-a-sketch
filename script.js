0eccbce6676e8377c966f68171370b23f0bbcee7
natalija-zdravkovic <201510014+natalija-zdravkovic@users.noreply.github.com>
2025-07-17 16:35:44 +0200

    Add makeGrid function with squares parameter Added function that creates a grid with squares*squares divs inside the .grid container

---
+++ script.js
@@ -0,0 +1,18 @@
+function makeGrid(squares)
+{
+    let grid = document.querySelector(".grid")
+    for (let i = 0; i < squares; i++)
+    {
+        let column = document.createElement("div")
+        column.classList.add("column")
+        for (let j = 1; j <= squares; j++)
+        {
+            let row = document.createElement("div")
+            row.classList.add("row")
+            row.style.border = "2px solid black"
+            row.innerText = (i * squares) + j;
+            column.appendChild(row)
+        }
+        grid.appendChild(column)
+    }
+}
